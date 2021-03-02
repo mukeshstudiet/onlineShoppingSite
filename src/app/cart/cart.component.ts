6773261fb88ebfcc5c59c90ff54173af4509250a
mukeshstudiet <34495083+mukeshstudiet@users.noreply.github.com>
2021-03-02 14:06:00 +0530

    Update cart.component.ts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -25,18 +25,38 @@ export class CartComponent {
   quantity:number=0;
 
   i=0;
-  plus(){
-    if(this.i !=10){
-      this.i++;
-      this.quantity = this.i;
+  
+   plus(product) {
+    if (product.quantity != 10) {
+      product.quantity = product.quantity + 1;
+      //  this.i++;
+      // this.quantity = this.i;
     }
   }
 
-  minus(){
-    if(this.i !=0){
-      this.i--;
-      this.quantity = this.i;
+  minus(product) {
+
+    if (product.quantity != 0) {
+      product.quantity = product.quantity - 1;
+      // this.i--;
+      //this.quantity = this.i;
     }
+
+  }
+  
+  
+    checkoutFinal() {
+
+    let total = 0,  //set a variable that holds our total
+      i;
+    for (i = 0; i < this.products.length; i++) {  //loop through the array
+      total += this.products[i].price * this.products[i].quantity;  //Do the math!
+    }
+
+    let orderFinal = { "orderDate": moment(new Date()).format('D MMM YYYY'), "orderCost": total.toFixed(2) }
+
+    console.log(orderFinal);
+
   }
 
 }
